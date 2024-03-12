6982b3272e6d12cc4b5c5a84837cb67ebbd5dce7
Dennsi <mr.dusky@outlook.de>
2024-03-12 16:53:14 +0100

    UE01 done

--- moneycounter.php
+++ moneycounter.php
@@ -25,25 +25,24 @@
     // TODO: Create a regular expression that satisfies the money counting conditions from README or the test string.
     // TODO: Start simple by using https://regex101.com/, paste the test string and start with a simple regex first.
     // TODO: Make your regex more extensive until it finds a few amounts of money in the text.
-    $pattern = "/^\d+(?:\.\d{2})?(?:.-)? EUR$/";
-    preg_match_all($pattern, $message, $matches);
+    $pattern = '/ (?!0\d)(\d+)(?:\.\d{2})?(?:.-)? EUR\b/';
+
+    if (preg_match_all($pattern, $message, $matches)) {
+        echo "Found " . count($matches[0]) . " matches:<br>";
+        foreach ($matches[0] as $match) {
+            $sumMoney += (float)$match;
+        }
+    }
     var_dump($matches);
-
-
     // TODO: Time to use the regex here in your code. Add a variable for the pattern and add it with a delimiter.
     // TODO: Run it on your input data. You need preg_match_all() because you need to find multiple occurrences of the pattern.
     // TODO: Be sure to add the 3rd parameter $matches so you can get the actual matched strings.
     // TODO: You can use var_dump() to see what $matches holds. https://www.phpliveregex.com/ also shows $matches output.
-    for ($i = 0; $i < count($matches[0]); $i++) {
-        $sumMoney += floatval($matches[$i]);
-    }
-
     // TODO: Build the sum of your found amounts of money. Create a foreach loop over your $matches array.
     // TODO: Add everything to your total sum. Since the regex returns strings, use floatval() to convert it to decimal numbers.
 
     // TODO: Last step: Return output.
     // TODO: Return the entered text and the total amount of money.
-    //return $sumMoney; -> Kein return-Statement, sonst wird nichts angezeigt
     // TODO: To convert the line breaks from the <textarea> (\n) to HTML linebreaks (<br>), use nl2br().
     // TODO: Use number_format() to format the total sum with two decimal digits, "," as thousands separator and "." as the decimal separator.
     echo "<p>" . number_format($sumMoney, 2,".", "," ) . "</p>";
